69dc1caa2bded335afd1214b9bf6d7b859347469
namanshandilyapsiborg <naman.shandilya@psiborg.in>
2022-08-31 13:20:52 +0530

    second

--- updater.js
+++ updater.js
@@ -3,9 +3,9 @@ const AutoGitUpdate = require('auto-git-update');
 const config = {
     repository: 'https://github.com/chegele/BackupPurger',
     fromReleases: true,
-    tempLocation: 'C:/Users/scheg/Desktop/tmp/',
+    tempLocation: 'D:/PsiBorg/',
     ignoreFiles: ['util/config.js'],
-    executeOnComplete: 'C:/Users/scheg/Desktop/worksapce/AutoGitUpdate/startTest.bat',
+    //executeOnComplete: 'C:/Users/scheg/Desktop/worksapce/AutoGitUpdate/startTest.bat',
     exitOnComplete: true
 }
 
